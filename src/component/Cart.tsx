7848cc593b28ef1a764874642f6fa233a7d06548
AZMOMAISON <mosesaizee@outlook.com>
2020-10-10 10:14:11 +0100

    Completed the project

--- src/component/Cart.tsx
+++ src/component/Cart.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { connect } from "react-redux";
+import { loadStripe } from "@stripe/stripe-js";
 import CartList from "./CartList";
 import { removeFromCart } from "../redux/action/cartAction";
 import { CartItems, Product, State } from "../redux/action/type";
 import formatMoney from "../util/formatMoney";
 import overAllTotal from "../util/overAllTotal";
+import { API_KEY } from "../config/stripe";
 import "../styles/Cart.css";
 
 interface Props {
@@ -12,11 +14,34 @@ interface Props {
   removeFromCart: (product: Product, cartItems: CartItems) => void;
 }
 
+const stripePromise = loadStripe(API_KEY);
+
 const Cart: React.FC<Props> = (props) => {
   const { cartItems, removeFromCart } = props;
 
   const handleRemoveItem = (product: Product) => {
     removeFromCart(product, cartItems);
+  };
+
+  const handleStripePayment = async () => {
+    // Get Stripe.js instance
+    const stripe = await stripePromise;
+
+    // Call your backend to create the Checkout Session
+    const response = await fetch("/create-checkout-session", { method: "POST" });
+
+    const session = await response.json();
+
+    // When the customer clicks on the button, redirect them to Checkout.
+    const result = await stripe!.redirectToCheckout({
+      sessionId: session.id,
+    });
+
+    if (result.error) {
+      // If `redirectToCheckout` fails due to a browser or network
+      // error, display the localized error message to your customer
+      // using `result.error.message`.
+    }
   };
 
   return (
@@ -32,7 +57,9 @@ const Cart: React.FC<Props> = (props) => {
         <div className="checkout">
           <h2>Total:</h2>
           <h2>{formatMoney(overAllTotal(cartItems))}</h2>
-          <button className="btn btn-secondary">Checkout</button>
+          <button className="btn btn-secondary" role="link" onClick={handleStripePayment}>
+            Checkout
+          </button>
         </div>
       )}
     </section>
